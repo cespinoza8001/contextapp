f488731a97880631f407ac523d2d0dbda3f7d34d
Christian <cespinoza8001@gmail.com>
2022-02-02 15:27:25 -0500

    added theme toggle and auth context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Navbar from "./Components/Navbar";
 import BookList from "./Components/BookList";
 import ThemeContextProvider from "./contexts/ThemeContext";
+import ThemeToggle from "./Components/ThemeToggle";
+import AuthContextProvider from "./contexts/AuthContext";
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-      <Navbar/>
-      <BookList/>
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
-
-
+import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends Component {
-    static contextType = ThemeContext;
-    render() { 
-        console.log(this.context);
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-        return (
+  render() {
+    return (
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
             <nav style={{ background: theme.ui, color: theme.syntax }}>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
+              <h1>Context App</h1>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
             </nav>
-        );
-    }
+          );
+        }}
+      </ThemeContext.Consumer>
+    );
+  }
 }
- 
-export default Navbar;
 
+export default Navbar;
